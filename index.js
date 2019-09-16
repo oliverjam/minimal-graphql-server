051696428765a4cc95b69ed00b0c00d05d2f8611
oliverjam <oliverjamesphillips@gmail.com>
2019-09-16 15:34:48 +0100

    Add variables support

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ const { graphql, buildSchema } = require("graphql");
 
 const schema = buildSchema(`
   type Query {
-    hello: String
+    hello(name: String!): String
   }
 `);
 
 const root = {
-  hello: () => "Hello world",
+  hello: variables => `Hello ${variables.name}`,
 };
 
 const server = createServer((request, response) => {
@@ -16,8 +16,10 @@ const server = createServer((request, response) => {
 
   const { searchParams } = new URL(request.url, "fake://");
   const query = searchParams.get("query");
+  const variables = JSON.parse(searchParams.get("variables"));
 
-  graphql(schema, query, root).then(result => {
+  graphql(schema, query, root, null, variables)
+    .then(result => {
     const { data } = result;
     response.end(JSON.stringify(data));
   });
