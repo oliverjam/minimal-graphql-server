66ba1e0880ec5b6453e13000f37e1fe19fc9f01d
oliverjam <oliverjamesphillips@gmail.com>
2019-09-16 14:11:57 +0100

    Add graphql

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
 const { createServer } = require("http");
+const { graphql, buildSchema } = require("graphql");
+
+const schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`);
+
+const root = {
+  hello: () => "Hello world",
+};
 
 const server = createServer((request, response) => {
   console.log(`${request.method} ${request.url}`);
-  response.end("Hello world");
+
+  graphql(schema, "{ hello }", root).then(result => {
+    const { data } = result;
+    response.end(JSON.stringify(data));
+  });
 });
 
 server.listen(3333, () => console.log("Listening on http://localhost:3333"));
