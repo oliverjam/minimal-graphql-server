91fe2e8e6a6a650095bbb1df1d42946c1eba9383
oliverjam <oliverjamesphillips@gmail.com>
2019-09-16 14:34:50 +0100

    Parse GET searchParams

--- index.js
+++ index.js
@@ -14,7 +14,10 @@ const root = {
 const server = createServer((request, response) => {
   console.log(`${request.method} ${request.url}`);
 
-  graphql(schema, "{ hello }", root).then(result => {
+  const { searchParams } = new URL(request.url, "fake://");
+  const query = searchParams.get("query");
+
+  graphql(schema, query, root).then(result => {
     const { data } = result;
     response.end(JSON.stringify(data));
   });
